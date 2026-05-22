6e332d745638d1838472b587a4660542767804fa
MoApplesNOranges <billmo0120@gmail.com>
2026-05-22 11:20:21 -0400

    docker added

--- models/customer.py
+++ models/customer.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 from models.account import Account
 
@@ -6,9 +6,9 @@ from models.account import Account
 class Customer(BaseModel):
     customer_id: str | int
     name: str
-    accounts: List[Account] = []
+    accounts: List[Account] = Field(default_factory=list)
 
 
 class CustomerCreate(BaseModel):
     name: str
-    accounts: List[Account] = []
+    accounts: List[Account] = Field(default_factory=list)
